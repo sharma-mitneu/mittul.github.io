273ce844a9bd5b2c86f9d0593672060bbcd8d91c
sharma-mitneu <145163557+sharma-mitneu@users.noreply.github.com>
2024-08-13 08:33:11 -0400

    adding changes

--- components/LatestCode.js
+++ components/LatestCode.js
@@ -5,39 +5,41 @@ import userData from "@constants/data";
 
 export default function LatestCode({ repositories }) {
   const [repos, setRepos] = useState(repositories);
+  const spacing = 10;
 
   return (
-    <section className="bg-[#F1F1F1] -mt-40 dark:bg-gray-900 pb-40">
+    <section className="bg-[#F1F1F1] -mt-40 dark:bg-gray-900 pb-30">
       <div className="max-w-6xl mx-auto">
-  <div className="flex flex-col md:flex-row justify-between items-center md:pt-40 mx-10">
-    <h1 className="text-6xl lg:text-9xl max-w-lg font-bold text-black my-20 md:my-0 mb-5 md:text-white dark:text-gray-600 text-center lg:text-left hover:bg-gray-200 dark:hover:bg-gray-800 transition-colors duration-300 rounded-md p-4 mb-20">
-      Latest Code
+      <div className="flex flex-col md:flex-row justify-between items-center md:pt-30 mx-10 space-y-10 md:space-y-0 md:space-x-10">
+    <h1 className="text-6xl lg:text-9xl max-w-lg font-bold text-black my-30 md:my-10 mb-5 md:text-black dark:text-gray-600 text-center lg:text-left rounded-md p-4 mb-20">
+        Latest Code
     </h1>
     <a
-      href={`https://github.com/${userData.githubUsername}`}
-      className="mb-20 md:mb-0 px-8 py-4 rounded-md bg-white shadow-lg text-xl font-semibold flex flex-row space-x-4 items-center dark:text-gray-700"
+        href={`https://github.com/${userData.githubUsername}`}
+        className="mb-20 md:mb-0 px-8 py-4 rounded-md bg-white shadow-lg text-xl font-semibold flex flex-row space-x-4 items-center dark:text-gray-700"
     >
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="16"
-        height="16"
-        fill="currentColor"
-        className="bi bi-arrow-up-right-square"
-        stroke="4"
-        strokeWidth="4"
-        viewBox="0 0 16 16"
-      >
-        <path
-          fillRule="evenodd"
-          d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm5.854 8.803a.5.5 0 1 1-.708-.707L9.243 6H6.475a.5.5 0 1 1 0-1h3.975a.5.5 0 0 1 .5.5v3.975a.5.5 0 1 1-1 0V6.707l-4.096 4.096z"
-        />
-      </svg>
-      <p>View GitHub</p>
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            className="bi bi-arrow-up-right-square"
+            stroke="4"
+            strokeWidth="4"
+            viewBox="0 0 16 16"
+        >
+            <path
+                fillRule="evenodd"
+                d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm5.854 8.803a.5.5 0 1 1-.708-.707L9.243 6H6.475a.5.5 0 1 1 0-1h3.975a.5.5 0 0 1 .5.5v3.975a.5.5 0 1 1-1 0V6.707l-4.096 4.096z"
+            />
+        </svg>
+        <p>View GitHub</p>
     </a>
-  </div>
+</div>
 </div>
 
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 max-w-6xl mx-auto px-10 lg:-mt-10 gap-y-20">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 max-w-6xl mx-auto px-10 lg:-mt-10 gap-y-30"
+    style={{ marginTop: spacing + 'px' }}>
         {/* Single github Repo */}
 
         {repos &&
